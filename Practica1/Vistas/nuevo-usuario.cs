c8dc484d9973c738aa5369380fcf7a9958d9c85f
Thomas <thomaswfds@gmail.com>
2024-01-09 13:27:53 +0100

    09/01/2023

---
+++ Practica1/Vistas/nuevo-usuario.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Practica1.manejadores;
+
+namespace Practica1.Vistas
+{
+    public partial class nuevo_usuario : Form
+    {
+        public nuevo_usuario()
+        {
+            InitializeComponent();
+        }
+
+        private void nuevo_usuario_Load(object sender, EventArgs e)
+        {
+            ControladorUsuariosXML.leerUsuariosXML();
+        }
+        private bool validar()
+        {
+            if (user.Text == "" || pass.Text == "" || pass.Text.Length < 4  || user.Text.Length < 4)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void vaciarCampos()
+        {
+            user.Text = "";
+            pass.Text = "";
+        }
+        private void camposNormal()
+        {
+            user.BackColor = Color.White; 
+            pass.BackColor = Color.White;
+        }
+        private void camposIncorrectos()
+        {
+            if (user.Text == "" || user.Text.Length < 4)
+            {
+                user.BackColor = Color.Red;
+            }
+            if (pass.Text == "" || pass.Text.Length < 4) 
+            {
+                pass.BackColor = Color.Red;
+
+            }
+        }
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (validar())
+            {
+                aniadirUsu();
+                vaciarCampos();
+                camposNormal();
+            }
+            else {
+                camposNormal();
+                camposIncorrectos();
+                MessageBox.Show("Revisa los campos, hay algún dato erróneo");
+            }
+        }
+        private void aniadirUsu()
+        {
+            Usuario u = new Usuario();
+
+            if (ControladorUsuariosXML.buscarUsuario(user.Text, pass.Text))
+            {
+                MessageBox.Show("Ese usuario ya existe");
+            }
+            else
+            {
+                u.User = user.Text;
+                u.Pass = pass.Text;
+                ControladorUsuariosXML.listaUsuarios.Add(u);
+                ControladorUsuariosXML.escribirUsuariosXML();
+                MessageBox.Show("Usuario registrado con éxito.");
+
+            }
+        }
+    }
+}
---
+++ Practica1/manejadores/ControladorUsuariosXML.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//importamos estas dependencias
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Practica1.manejadores
+{
+    //Importante que los métodos de las clases controladoras y las clases sean public y static
+    public static class ControladorUsuariosXML
+    {
+        public static List<Usuario> listaUsuarios = new List<Usuario>();
+
+        public static void escribirUsuariosXML()
+        {
+            try
+            {
+                using (var writer = new StreamWriter("usuarios.xml"))
+                {
+                    // Do this to avoid the serializer inserting default XML namespaces.
+                    var namespaces = new XmlSerializerNamespaces();
+                    namespaces.Add(string.Empty, string.Empty);
+                    var serializer = new XmlSerializer(listaUsuarios.GetType());
+                    serializer.Serialize(writer, listaUsuarios, namespaces);
+                }
+            }
+            catch (Exception e) {
+                
+            }
+        }
+
+        public static void leerUsuariosXML()
+        {
+            try
+            {
+                string xml = File.ReadAllText("usuarios.xml");
+                using (var reader = new StringReader(xml))
+                {
+                    XmlSerializer serializer = new XmlSerializer(listaUsuarios.GetType());
+                    listaUsuarios = (List<Usuario>) serializer.Deserialize(reader);
+                }
+            }
+
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        public static bool validaLogin(ref string usuario, ref string clave)
+        {
+            for (int i = 0; i < listaUsuarios.Count; i++)
+            {
+                if ((usuario == listaUsuarios[i].User.ToLower())
+                    && (clave == listaUsuarios[i].Pass))
+                {
+                    Usuario.u = new Usuario(usuario, clave);
+                    return true;
+                }
+                else if ((usuario != listaUsuarios[i].User.ToLower())
+                    || (clave != listaUsuarios[i].Pass.ToLower()))
+                {
+                    continue;
+                }
+            }
+            MessageBox.Show("Usuario o contraseña incorrectos");
+            return false;
+        }
+
+        public static bool buscarUsuario(string usuario, string pass)
+        {
+            for (int i = 0; i < listaUsuarios.Count; i++)
+            {
+                if (usuario.ToLower() == listaUsuarios[i].User.ToLower())
+                {
+                    if (pass == listaUsuarios[i].Pass)
+                    {
+                        return true;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void cargarUsuarios()
+        {
+            Usuario u = new Usuario("root", "1234");
+            listaUsuarios.Add(u);
+            u = new Usuario("jorge", "4321");
+            listaUsuarios.Add(u);
+            u = new Usuario("jaime", "4444");
+            listaUsuarios.Add(u);
+            u = new Usuario("jose", "1111");
+            listaUsuarios.Add(u);
+            u = new Usuario("javier", "2222");
+            listaUsuarios.Add(u);
+        }
+    }
+}
